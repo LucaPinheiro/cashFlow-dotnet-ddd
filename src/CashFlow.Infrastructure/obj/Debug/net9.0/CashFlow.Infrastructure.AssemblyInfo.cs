884c6ee5c2a3e2ef4fa9b5f3b415e9cbcf5b7b9b
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-07-30 20:09:11 -0300

    infra

--- src/CashFlow.Infrastructure/obj/Debug/net9.0/CashFlow.Infrastructure.AssemblyInfo.cs
+++ src/CashFlow.Infrastructure/obj/Debug/net9.0/CashFlow.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc31d1ba0a28efff94c0374810c46d50d9bc68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2833c42ba52194a9e3ecb30353a23902beef4cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
