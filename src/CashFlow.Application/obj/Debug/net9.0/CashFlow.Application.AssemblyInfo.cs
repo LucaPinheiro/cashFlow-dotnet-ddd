fe161e05699f0e7abaa25937f4c26fcd14685059
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-08-03 17:17:24 -0300

    PasswordValidator UT

--- src/CashFlow.Application/obj/Debug/net9.0/CashFlow.Application.AssemblyInfo.cs
+++ src/CashFlow.Application/obj/Debug/net9.0/CashFlow.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b482730af9b308f47fc6a4d5090be97bf0861b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8519ee6716c6be474a9f8db0884f35696a04a785")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
