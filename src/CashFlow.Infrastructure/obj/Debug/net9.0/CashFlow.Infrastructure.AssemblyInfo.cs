884e83834242119cd3810df86cf8fdf37eea0c29
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-08-10 17:00:43 -0300

    adjusting

--- src/CashFlow.Infrastructure/obj/Debug/net9.0/CashFlow.Infrastructure.AssemblyInfo.cs
+++ src/CashFlow.Infrastructure/obj/Debug/net9.0/CashFlow.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e897a36226e0f5bf97f1fc22b809aa3d388ca51c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a973b61ff2ddf37f3cff6d61eee03371544139b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
