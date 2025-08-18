e3cee325d211952cac51ad7ae51b7b0f9203e89c
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-08-17 21:10:54 -0300

    Integration test and configuration web service test environ

--- WebApi.Test/Users/Register/RegisterUserTest.cs
+++ WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,12 +1,28 @@
+using System.Net;
+using System.Net.Http.Json;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace WebApi.Test.Users.Register;
 
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 {
+    private const string METHOD = "api/User";
+    private readonly HttpClient _httpClient;
+    public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();
+    }
+
     [Fact]
     public async Task Success()
     {
-        
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var response = await _httpClient.PostAsJsonAsync(requestUri: METHOD, value: request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
     }
 }
