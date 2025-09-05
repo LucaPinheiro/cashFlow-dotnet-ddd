ca0b3c3926b65cee418f4266054e79d3d65cc251
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-05 16:04:35 -0300

    adjusts

--- src/CashFlow.API/obj/Debug/net9.0/CashFlow.AssemblyInfo.cs
+++ src/CashFlow.API/obj/Debug/net9.0/CashFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6c4d14feb4b0a04eed2cde449d364296c7f270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ceb2a112d190ad624c65ebbbb9f424f70b57ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CashFlow.Infrastructure/obj/Debug/net9.0/CashFlow.Infrastructure.AssemblyInfo.cs
+++ src/CashFlow.Infrastructure/obj/Debug/net9.0/CashFlow.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265de353284a3323c874686e3caff343d40f9ec0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ceb2a112d190ad624c65ebbbb9f424f70b57ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
